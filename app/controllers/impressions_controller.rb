662e3e257772da8b32298108099e5210210e0237
daiki-fffff <watanuki_daiki1_4@yahoo.co.jp>
2020-07-29 07:01:55 +0900

    edit impressions_controller

--- app/controllers/impressions_controller.rb
+++ app/controllers/impressions_controller.rb
@@ -2,4 +2,44 @@ class ImpressionsController < ApplicationController
   def index
     @impressions = Impression.all
   end
+
+  def show
+    @impression = Impression.find(params[:id])
+  end
+
+  def new 
+    if user_signed_in?
+      @impression = Impression.new
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
+  def create
+    @impression = Impression.new(impression_params)
+    @impression.save
+    if @impression.save
+      redirect_to root_url
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @impression.update(impression_params)
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    if @impression.user_id == current_user.id && @impression.destroy
+      flash[:notice] = "削除が完了しました。"
+      redirect_to root_url
+    else
+      flash[:alert] = "削除できませんでした。"
+      redirect_to root_url
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'tops/index'
+  root to: 'tops#index'
   resources :impressions, only:[:index, :create]
   resources :tops, only:[:index]
   get '/users/logout', 'users#logout'
