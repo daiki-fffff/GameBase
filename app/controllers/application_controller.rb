a501cfa634cee142d59dfcd997b465f17a0946ed
daiki-fffff <watanuki_daiki1_4@yahoo.co.jp>
2020-08-28 07:09:48 +0900

    edit-nickname-sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,14 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    added_attrs = [ :nickname,
-                    :email,
-                    :password,
-                    :password_confirmation,
-                  ]             
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+    # added_attrs = [ :nickname,
+    #                 :email,
+    #                 :password,
+    #                 :password_confirmation,
+    #               ]             
+    # devise_parameter_sanitizer.permit ([:sign_up, keys: added_attrs])
+    # devise_parameter_sanitizer.permit ([:account_update, keys: added_attrs])
+    # devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 end
