97a993daed64caa26b8685c08a54d485682bb556
daiki-fffff <watanuki_daiki1_4@yahoo.co.jp>
2020-08-28 05:46:25 +0900

    edit-delete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_23_031117) do
+ActiveRecord::Schema.define(version: 2020_08_02_131950) do
 
   create_table "impressions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -44,7 +44,6 @@ ActiveRecord::Schema.define(version: 2020_08_23_031117) do
     t.string "nickname"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
