1b2aace504bb44fb52a7b102a822b512ebecba18
daiki-fffff <watanuki_daiki1_4@yahoo.co.jp>
2020-08-28 07:30:34 +0900

    edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,11 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    # added_attrs = [ :nickname,
-    #                 :email,
-    #                 :password,
-    #                 :password_confirmation,
-    #               ]             
+    added_attrs = [ :nickname,
+                    :email,
+                    :password,
+                    :password_confirmation,
+                  ]             
     # devise_parameter_sanitizer.permit ([:sign_up, keys: added_attrs])
     # devise_parameter_sanitizer.permit ([:account_update, keys: added_attrs])
     # devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
